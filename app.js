0565e5887d8e40934f279a1f8bb8832f8bc7ea72
ERWANCEN <erwance@hotmail.fr>
2025-01-12 18:14:17 +0100

    Fonctionnalités adresse : OK

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ let prenomErreur = document.getElementById('message_erreur_prenom');
 let email = document.getElementById('email');
 let emailErreur = document.getElementById('message_erreur_email');
 
+let date = document.getElementById('date');
+let dateErreur = document.getElementById('message_erreur_date');
+
+let adresse = document.getElementById('adresse');
+let adresseErreur = document.getElementById('message_erreur_adresse');
+
 let erreur = [];
 let erreurTypeString = '';
 let erreursSansDoublons = [];
@@ -21,18 +27,13 @@ let erreursSansDoublons = [];
  * Fonction permettant l'affichage des erreurs liées au nom
  */
 function afficherErreurNom() {
-    console.log('ccc');
     
     if(erreurTypeString.length > 0){
-        console.log('ddd');
-        
         nomErreur.innerHTML = 
             `
             <p class="erreur">${erreurTypeString}</p>
             `
     } else {
-        console.log('eee');
-        
         nomErreur.innerHTML = 
             `
             <p></p>
@@ -62,7 +63,7 @@ function afficherErreurPrenom() {
 
 
 /**
- * Fonction permettant l'affichage des erreurs liées au prénom
+ * Fonction permettant l'affichage des erreurs liées au email
  */
 function afficherErreurEmail() {
     if(erreurTypeString.length > 0){
@@ -72,6 +73,44 @@ function afficherErreurEmail() {
             `
     } else {
         emailErreur.innerHTML = 
+            `
+            <p></p>
+            `
+    }
+}
+
+
+
+/**
+ * Fonction permettant l'affichage des erreurs liées à la date
+ */
+function afficherErreurDate() {
+    if(erreurTypeString.length > 0){
+        dateErreur.innerHTML = 
+            `
+            <p class="erreur">${erreurTypeString}</p>
+            `
+    } else {
+        dateErreur.innerHTML = 
+            `
+            <p></p>
+            `
+    }
+}
+
+
+
+/**
+ * Fonction permettant l'affichage des erreurs liées à l'adresse
+ */
+function afficherErreurAdresse() {
+    if(erreurTypeString.length > 0){
+        adresseErreur.innerHTML = 
+            `
+            <p class="erreur">${erreurTypeString}</p>
+            `
+    } else {
+        adresseErreur.innerHTML = 
             `
             <p></p>
             `
@@ -106,6 +145,14 @@ bouton.addEventListener('click', (event) => {
     verificationsEmail();
     afficherErreurEmail();
     refreshErreur();
+
+    verificationsDate();
+    afficherErreurDate();
+    refreshErreur();
+
+    verificationsAdresse();
+    afficherErreurAdresse();
+    refreshErreur();
     
 })
 
@@ -118,15 +165,14 @@ function verificationsNom() {
 
     switch (true) {
         case (nom.value.trim().length < 5 || nom.value.trim().length > 15):
-            erreur.push('Votre nom doit comprendre entre 5 et 15 caractères');
+            erreur.push('Votre nom doit contenir entre 5 et 15 caractères');
             // console.log(erreur);
             if (nom.value.trim().length > 0) {
                 for (let i = 0; i < nom.value.length; i++) {
-                    
     
                     if (nom.value[i] == 0 || nom.value[i] == 1 || nom.value[i] == 2 || nom.value[i] == 3 || nom.value[i] == 4 || nom.value[i] == 5 || nom.value[i] == 6 || nom.value[i] == 7 || nom.value[i] == 8 || nom.value[i] == 9) {
 
-                        erreur.push(' Votre nom ne doit pas comporter de chiffres');
+                        erreur.push(' Votre nom ne doit pas contenir de chiffres');
                         // console.log(erreur);
                         
                     }
@@ -145,7 +191,7 @@ function verificationsNom() {
 
                 if (nom.value[i] == '0' || nom.value[i] == '1' || nom.value[i] == '2' || nom.value[i] == '3' || nom.value[i] == '4' || nom.value[i] == '5' || nom.value[i] == '6' || nom.value[i] == '7' || nom.value[i] == '8' || nom.value[i] == '9') {
 
-                    erreur.push('Votre nom ne doit pas comporter de chiffres');
+                    erreur.push('Votre nom ne doit pas contenir de chiffres');
                     // console.log(erreur);
                     
                 }
@@ -163,9 +209,11 @@ function verificationsNom() {
             erreurTypeString = erreursSansDoublons.toString();
             // console.log(erreurTypeString);
             break;
+
     }
 
 }
+
 
 
 /**
@@ -176,7 +224,7 @@ function verificationsPrenom() {
 
     switch (true) {
         case (prenom.value.trim().length < 5 || prenom.value.trim().length > 15):
-            erreur.push('Votre prénom doit comprendre entre 5 et 15 caractères');
+            erreur.push('Votre prénom doit contenir entre 5 et 15 caractères');
             // console.log(erreur);
             if (prenom.value.trim().length > 0) {
                 for (let i = 0; i < prenom.value.length; i++) {
@@ -184,13 +232,13 @@ function verificationsPrenom() {
     
                     if (prenom.value[i] == 0 || prenom.value[i] == 1 || prenom.value[i] == 2 || prenom.value[i] == 3 || prenom.value[i] == 4 || prenom.value[i] == 5 || prenom.value[i] == 6 || prenom.value[i] == 7 || prenom.value[i] == 8 || prenom.value[i] == 9) {
 
-                        erreur.push(' Votre prénom ne doit pas comporter de chiffres');
+                        erreur.push(' Votre prénom ne doit pas contenir de chiffres');
                         // console.log(erreur);
                         
                     }
-                    
                 }
             }
+
             erreursSansDoublons = [...new Set(erreur)];
             // console.log(erreursSansDoublons);
             erreurTypeString = erreursSansDoublons.toString();
@@ -203,12 +251,12 @@ function verificationsPrenom() {
 
                 if (prenom.value[i] == '0' || prenom.value[i] == '1' || prenom.value[i] == '2' || prenom.value[i] == '3' || prenom.value[i] == '4' || prenom.value[i] == '5' || prenom.value[i] == '6' || prenom.value[i] == '7' || prenom.value[i] == '8' || prenom.value[i] == '9') {
 
-                    erreur.push('Votre prénom ne doit pas comporter de chiffres');
+                    erreur.push('Votre prénom ne doit pas contenir de chiffres');
                     // console.log(erreur);
                     
                 }
-                
             }
+
             erreursSansDoublons = [...new Set(erreur)];
             erreurTypeString = erreursSansDoublons.toString();
             // console.log(erreurTypeString);
@@ -221,20 +269,15 @@ function verificationsPrenom() {
             erreurTypeString = erreursSansDoublons.toString();
             // console.log(erreurTypeString);
             break;
+
     }
 }
 
 
 
-// function verificationFormatEmail(email) {
-//     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//     // return regex.test(email);
-//     console.log(regex.test(email));
-    
-    
-// }
-
-
+/**
+ * Function permettant de vérifier différents critères lors de la saisie de la partie "Email"
+ */
 function verificationsEmail() {
 
     // Les emails bannis sont les plus utilisés, mais l'utilisation d'APIs par la suite pour gérer ça sera recommandé
@@ -258,7 +301,7 @@ function verificationsEmail() {
         let domaine = email.value.split('@')[1];
 
         if (emailsBannis.includes(domaine)) {
-            erreur.push('Les emails provenant de ce domaine sont interdits');
+            erreur.push('Les emails provenant de ce domaine sont interdites');
 
         }
     }
@@ -266,4 +309,61 @@ function verificationsEmail() {
     erreursSansDoublons = [...new Set(erreur)];
     erreurTypeString = erreursSansDoublons.toString();
 
+}
+
+
+
+/**
+ * Function permettant de vérifier différents critères lors de la saisie de la partie "Date"
+ */
+function verificationsDate() {
+
+    console.log(date.valueAsDate);
+    console.log(date.value);
+
+    if (date.valueAsDate == null) {
+        erreur.push('Veuillez sélectionner une date');
+    } else {
+        let dateAjd = new Date();
+        if(dateAjd < date.valueAsDate){
+            erreur.push('Une date de naissance ne peut pas être ultérieure au jour même');
+        }
+    }
+    
+    erreursSansDoublons = [...new Set(erreur)];
+    erreurTypeString = erreursSansDoublons.toString();
+}
+
+
+
+/**
+ * Function permettant de vérifier différents critères lors de la saisie de la partie "Adresse"
+ */
+function verificationsAdresse() {
+    
+    if (adresse.value.length < 5 || adresse.value.length > 200) {
+        erreur.push("L'adresse doit contenir entre 5 et 200 caractères");
+        console.log('longueur')
+    }
+
+    if (!/[a-zA-Z]/.test(adresse.value)) {
+        erreur.push("L'adresse doit contenir au moins une lettre");
+        console.log('lettres');
+        
+    }
+            
+    if (!/\d/.test(adresse.value)) {
+        erreur.push("L'adresse doit contenir au moins un chiffre");
+        console.log('chiffre');
+        
+    }
+
+    if (/[^a-zA-Z0-9\s,.'\-]/.test(adresse.value)){ // caractères valides : a-zA-Z / 0-9 /   / , / . / ' / - 
+        erreur.push("L'adresse contient des caractères non valides");
+        console.log('caractères');
+        
+    }
+    
+    erreursSansDoublons = [...new Set(erreur)];
+    erreurTypeString = erreursSansDoublons.toString();
 }
